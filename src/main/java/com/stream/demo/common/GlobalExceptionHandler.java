709156ddc48e4304c7b3fa6d1a0f07cbba8c52bb
phanconghung200591 <phanconghung200591@hmail.com>
2025-12-17 16:26:20 +0900

    feat: Set up initial backend configuration, development environment variables, and global exception handling.

--- src/main/java/com/stream/demo/common/GlobalExceptionHandler.java
+++ src/main/java/com/stream/demo/common/GlobalExceptionHandler.java
@@ -1,15 +1,18 @@
 package com.stream.demo.common;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
+		log.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ApiResponse.error(e.getMessage()));
     }
